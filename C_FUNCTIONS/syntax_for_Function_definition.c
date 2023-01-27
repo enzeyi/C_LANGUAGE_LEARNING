a77ba95c3aa0b0480506726bab31457c25bdf447
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 16:18:02 +0300

    Create syntax_for_Function_definition.c

---
+++ C_FUNCTIONS/syntax_for_Function_definition.c
@@ -0,0 +1,4 @@
+return_type function_name(parameter_type parameter_name) {
+    // function body
+}
+//as you have seen the function definition contains the function declarations + the function body

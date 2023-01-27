bd85e20bb22370c3e62bdf51480d6f19a000cdac
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 17:08:53 +0300

    Create Declaring_variable_pointer.c

---
+++ C_POINTERS/Declaring_variable_pointer.c
@@ -0,0 +1,8 @@
+//first we create an integer variable and name it x,then on the same line we assign it a value of 5
+int x = 5;
+
+//then we create an integer pointer then name the pointer as ptr
+int *ptr;
+
+//then we assign the address to the variable to our ponter as follows
+ptr = &x;

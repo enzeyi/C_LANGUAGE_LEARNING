f7c98ff77e8380b8414aafe2b33f0272bee8704a
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 16:23:57 +0300

    Create sample_function_declaration-definition_and_calling.c

---
+++ C_FUNCTIONS/sample_function_declaration-definition_and_calling.c
@@ -0,0 +1,16 @@
+//first we define our function
+
+int add(int a, int b) {
+    return a + b;
+}
+
+//then next we are going to use it, by calling it in our program.
+
+//we will store the returned value in a variable then print it out
+
+int main() {
+    int result = add(3, 4);
+    printf("%d", result);
+    return 0;
+}
+

6413811403faa42da7b3e1d565c44de75aac44da
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 17:43:51 +0300

    Create array_variable_syntax.c

---
+++ C_VARIABLES/array_variable_syntax.c
@@ -0,0 +1,8 @@
+//This is the syntax for creating an array variable in c
+data_type array_name[size];
+
+//For example, to create an array variable called "numbers" that can hold 5 integers
+int numbers[5];
+
+//You can also initialize the array with values at the time of declaration:
+int numbers[5] = {1, 2, 3, 4, 5};

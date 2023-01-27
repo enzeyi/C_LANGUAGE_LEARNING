f59da576a6827762dc0106fd25a115ba163f91d3
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 16:38:58 +0300

    Create variable_syntax.c

---
+++ C_VARIABLES/variable_syntax.c
@@ -0,0 +1,19 @@
+data_type variable_name;
+
+//For example, the following code declares an integer variable x
+
+int x;
+
+//You can also initialize a variable at the same time it's declared by assigning it a value using the assignment operator (=)
+
+int x = 5;
+
+//You can also declare multiple variables of the same data type in a single statement using a comma-separated list.
+
+int x = 5, y = 10, z;
+
+//It's important to note that C is a typed language, this means that a variable can only hold a value of its declared data type. 
+//Attempting to assign a value of a different data type to a variable will result in a compile-time error.
+
+//It's also important to give meaningful names to variables and make sure they are used consistently throughout the program to make it more readable and maintainable.
+//It's also important to keep track of the scope of the variables, this means where they are accessible in the program, and make sure they are not being accessed outside of it.

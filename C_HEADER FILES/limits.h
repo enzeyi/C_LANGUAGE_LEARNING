1f6695eae6feea277188cb5f4d29ea869b9f8272
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 15:35:20 +0300

    Update and rename limit.h to limits.h

---
+++ C_HEADER FILES/limits.h
@@ -0,0 +1,5 @@
+//limits.h is a standard C library header that defines various limits and parameters for the implementation of the C language. 
+//These limits include the maximum and minimum values for various data types (such as INT_MAX for the maximum value of an int), as well as other implementation-specific details (such as the number of bits in a byte).
+
+//The contents of limits.h are specified by the C standard and should be present in any C implementation. 
+//It's commonly used by the programmer in order to write portable code that can work on different platforms with different characteristics.

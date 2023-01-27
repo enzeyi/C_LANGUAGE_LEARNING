38c27fb11cc54ce6718185872859ccb978fed84d
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 15:44:10 +0300

    Create stdio.h

---
+++ C_HEADER FILES/stdio.h
@@ -0,0 +1,9 @@
+//stdio.h is a standard C library header that provides access to a number of functions for performing input and output (I/O) operations. These functions include:
+
+//printf() and sprintf() for printing to the console or to a string
+//scanf() and fscanf() for reading input from the console or from a file
+//fopen(), fclose(), fread(), and fwrite() for working with files
+//getchar() and putchar() for reading and writing individual characters
+
+//stdio.h is a very important library in C, it provides a lot of functionality to the programmer to work with the input and output, it's widely used in many C programs. 
+//The functions provided in stdio.h are defined in the C standard and should be present in any C implementation.

ecdbadf1b4b24938ee4286b38b1631b35dff4dd0
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-27 16:16:11 +0300

    Create syntax_for_Function_ declaration-or-function_prototype.c

---
+++ C_FUNCTIONS/syntax_for_Function_ declaration-or-function_prototype.c
@@ -0,0 +1,2 @@
+return_type function_name(parameter_type parameter_name,parameter_type parameter_name,parameter_type parameter_name, ...);
+//you can have as many parameter types plus their names as possible 
